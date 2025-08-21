70f2f0399ede8b41e6c4f6e08253948b01abcc21
FrankSteps <franciscopassos.contato@gmail.com>
2025-08-20 22:15:03 -0300

    no namespaces!

--- disco.cpp
+++ disco.cpp
@@ -34,10 +34,7 @@
  qualquer outra necessidade para o programa. Após todo o processo, o programa vai poder ser usado normalmente.
 
 
- English [EN-USA]
- Traduza por conta própria, que tal? Ou não sabe usar um  tradutor?!
- Tem o duolingo caso queira um caminho mais difícil. Aprende português logo, é legal. Precisei dela para
- passar no meu vestibular e ingressar na faculdade de CC (significa "Cheiro Corporal").
+ English [EN-USA]:
  */
 
 //bibliotecas utilizadas no projeto
@@ -47,32 +44,28 @@
 #include <string>
 #include <cmath>
 
-//declarando os namespaces
-using namespace std;
-using namespace sf;    
-
 //variáveis para o uso dos botões
 bool playing = false; 
 int back_adv = 0;
 int currentSong = 0;
 
 int main() {
-    RenderWindow window(VideoMode(294, 211), "Disco - Frank Steps"); 
-    Clock rainbowClock; 
+    sf::RenderWindow window(sf::VideoMode(294, 211), "Disco - Frank Steps", sf::Style::Default); 
+    sf::Clock rainbowClock; 
 
     //bloco de códigos responsáveis pelos textos que estão no projeto
-    Font pixel;
+    sf::Font pixel;
     pixel.loadFromFile("fonts/pixel_art.ttf");
 
-    Text songName;
+    sf::Text songName;
     songName.setFont(pixel);
     songName.setCharacterSize(14);
-    songName.setFillColor(Color::White);
+    songName.setFillColor(sf::Color::White);
     songName.setPosition(5, 140);
     songName.setString("By Frank Steps");
 
     //bloco de códigos responsáveis pelas imagens do projeto
-    Image 
+    sf::Image 
         icon, play, pause, advenced, back
     ;
 
@@ -80,7 +73,7 @@ int main() {
     icon.loadFromFile ("image/icon/icon.png");
     window.setIcon (icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 
-    Texture 
+    sf::Texture 
         texCyan, texPurple, texPink, texGreen, texCooper, texNothing, 
         texPlay, texBack, texPause, texAdvenced
     ;
@@ -97,15 +90,15 @@ int main() {
     texPause.loadFromFile ("image/buttons/pause_button.png");
     texAdvenced.loadFromFile ("image/buttons/to_advenced_button.png");
 
-    Sprite 
+    sf::Sprite 
         sCyan (texCyan), sPurple (texPurple), sPink (texPink), sGreen (texGreen), 
         sCooper (texCooper), sNothing (texNothing), sPlay(texPlay), sBack(texBack), 
         sPause(texPause), sAdvenced(texAdvenced)
     ;
 
     //ponteiros dos sprites
-    Sprite* currentSprite = nullptr;
-    Sprite* currentPlayButton = &sPlay;
+    sf::Sprite* currentSprite = nullptr;
+    sf::Sprite* currentPlayButton = &sPlay;
 
     //posicionando os elementos/imagens na tela antes de mostrá-las
     sBack.setPosition(62, 176);
@@ -114,7 +107,7 @@ int main() {
     sAdvenced.setPosition(210, 176);
 
     //Bloco responsável pelos sons do projeto
-    SoundBuffer 
+    sf::SoundBuffer 
         bufferNeod, bufferMush, bufferWorld, bufferSweet, bufferCooper
     ;
 
@@ -124,7 +117,7 @@ int main() {
     bufferSweet.loadFromFile ("sounds/sweet.wav");
     bufferCooper.loadFromFile ("sounds/cooper.wav");
 
-    Sound 
+    sf::Sound 
         Neod, Mush, World, Sweet, Cooper
     ;
 
@@ -135,15 +128,15 @@ int main() {
     Cooper.setBuffer (bufferCooper);
 
     //Loop principal - Aqui é onde todo o software é gerenciado
-    Event event;
+    sf::Event event;
     while (window.isOpen()) {
         while (window.pollEvent(event)) {
-            if (event.type == Event::Closed){
+            if (event.type == sf::Event::Closed){
                 window.close();
             }
 
             if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left){
-                Vector2f mousePos = window.mapPixelToCoords(Mouse::getPosition(window));
+                sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
 
                 if(sAdvenced.getGlobalBounds().contains(mousePos)){
                     if(back_adv < 5){
@@ -185,27 +178,27 @@ int main() {
                 Sweet.stop();
 
                 if(back_adv == 1){
-                    cout << "Tocando Neodymium - Frank Steps\n";
+                    std::cout << "Tocando Neodymium - Frank Steps\n";
                     songName.setString("Neodymium - Frank Steps");
                     currentSprite = &sCyan;
                     Neod.play();
                 } else if (back_adv == 2) {
-                    cout << "Tocando Mushrooms - Frank Steps\n";
+                    std::cout << "Tocando Mushrooms - Frank Steps\n";
                     songName.setString("Mushrooms - Frank Steps");
                     currentSprite = &sPurple;
                     Mush.play();
                 } else if (back_adv == 3) {
-                    cout << "Tocando Neo World - Frank Steps\n";
+                    std::cout << "Tocando Neo World - Frank Steps\n";
                     songName.setString("Neo World - Frank Steps");
                     currentSprite = &sPink;
                     World.play();
                 } else if (back_adv == 4) {
-                    cout << "Tocando Bright Cooper - Frank Steps\n";
+                    std::cout << "Tocando Bright Cooper - Frank Steps\n";
                     songName.setString("Bright Cooper - Frank Steps");
                     currentSprite = &sCooper;
                     Cooper.play();
                 } else if (back_adv == 5) {
-                    cout << "Tocando Sweet Childhood - Frank Steps\n";
+                    std::cout << "Tocando Sweet Childhood - Frank Steps\n";
                     songName.setString("Sweet Childhood - Frank Steps");
                     currentSprite = &sGreen;
                     Sweet.play();
@@ -221,7 +214,7 @@ int main() {
             }
         }
         // Exibição da imagem atual
-        window.clear(Color::Black);
+        window.clear(sf::Color::Black);
         if (currentSprite) {
             window.draw(*currentSprite);
         } else {
@@ -229,10 +222,10 @@ int main() {
         }
         //efeito arco-iris por intermédio das funções trigonométricas - estudar mais sobre a biblioteca cmath
         float time = rainbowClock.getElapsedTime().asSeconds();
-        Uint8 r = static_cast<Uint8>(127 + 127 * sin(2 * 3.141590 * time * 0.3));
-        Uint8 g = static_cast<Uint8>(127 + 127 * sin(2 * 3.141590 * time * 0.3 + 2));
-        Uint8 b = static_cast<Uint8>(127 + 127 * sin(2 * 3.141590 * time * 0.3 + 4));
-        songName.setFillColor(Color(r, g, b));
+        sf::Uint8 r = static_cast<sf::Uint8>(127 + 127 * sin(2 * 3.141590 * time * 0.3));
+        sf::Uint8 g = static_cast<sf::Uint8>(127 + 127 * sin(2 * 3.141590 * time * 0.3 + 2));
+        sf::Uint8 b = static_cast<sf::Uint8>(127 + 127 * sin(2 * 3.141590 * time * 0.3 + 4));
+        songName.setFillColor(sf::Color(r, g, b));
 
         //por fim, mostrando os elementos na tela
         window.draw(songName);
